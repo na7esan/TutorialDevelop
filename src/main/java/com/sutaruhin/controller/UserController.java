f208d0f5b90e133458ab2402a6ec688abe749954
na7esan <na7esan@gmail.com>
2023-06-29 00:57:31 +0900

    Chapter 8.6 プログラムの実行。CRUD処理のCUDの実装。次はChapter9.1サービスの修正

--- src/main/java/com/sutaruhin/controller/UserController.java
+++ src/main/java/com/sutaruhin/controller/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sutaruhin.entity.User;
 import com.sutaruhin.service.UserService;
 
 @Controller
@@ -22,4 +26,35 @@ public class UserController {
 
         return "user/list";
     }
+
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(User user) {
+
+        service.saveUser(user);
+
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/update/{id}/")
+    public String getUser(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("user", service.getUser(id));
+
+        return "user/update";
+    }
+
+    @PostMapping("/update/{id}/")
+    public String postUser(User user) {
+        service.saveUser(user);
+
+        return "redirect:/user/list";
+    }
+
+
 }
--- src/main/java/com/sutaruhin/service/UserService.java
+++ src/main/java/com/sutaruhin/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sutaruhin.entity.User;
 import com.sutaruhin.repository.UserRepository;
@@ -17,5 +18,14 @@ public class UserService {
     public List<User> getUserList() {
 
         return userRepository.findAll();
+    }
+
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 }
