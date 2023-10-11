d8e005dd7999e5ca8fa5984bee1c84e23ab52194
na7esan <na7esan@gmail.com>
2023-10-11 20:56:43 +0900

    Chapter 9.5 プログラムの実行まで完了

--- src/main/java/com/sutaruhin/controller/UserController.java
+++ src/main/java/com/sutaruhin/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.sutaruhin.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sutaruhin.entity.User;
 import com.sutaruhin.service.UserService;
@@ -54,6 +57,14 @@ public class UserController {
         service.saveUser(user);
 
         return "redirect:/user/list";
+    }
+
+    @PostMapping(path="list",params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+
+    	service.deleteUser(idck);
+
+    	return "redirect:/user/list";
     }
 
 
