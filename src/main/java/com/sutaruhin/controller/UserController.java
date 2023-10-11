25018f3a4d0a4bae2a6453331910f9afbcfc25fa
na7esan <na7esan@gmail.com>
2023-10-11 22:41:42 +0900

    Chapter 10.7 プログラムの実行のバリデーションまで完了

--- src/main/java/com/sutaruhin/controller/UserController.java
+++ src/main/java/com/sutaruhin/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,19 +32,28 @@ public class UserController {
         return "user/list";
     }
 
+
     @GetMapping("/register")
     public String getRegister(@ModelAttribute User user) {
 
         return "user/register";
     }
 
+
+
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+
+            return getRegister(user);
+        }
 
         service.saveUser(user);
 
         return "redirect:/user/list";
     }
+
+
 
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
@@ -52,20 +63,21 @@ public class UserController {
         return "user/update";
     }
 
+
     @PostMapping("/update/{id}/")
     public String postUser(User user) {
+
         service.saveUser(user);
 
         return "redirect:/user/list";
     }
 
-    @PostMapping(path="list",params="deleteRun")
+
+    @PostMapping(path="list", params="deleteRun")
     public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
 
-    	service.deleteUser(idck);
+        service.deleteUser(idck);
 
-    	return "redirect:/user/list";
+        return "redirect:/user/list";
     }
-
-
 }
